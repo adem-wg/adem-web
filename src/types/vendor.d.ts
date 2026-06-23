ce9b6bdb8705d4a5674a6a3b462823e2304d3ade
Felix Linker <linkerfelix@gmail.com>
2026-06-23 11:15:31 +0200

    Remove stupid code

--- src/types/vendor.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '@lapo/asn1js' {
-  const ASN1: {
-    decode(input: Uint8Array | ArrayBuffer | number[]): unknown;
-  };
-  export default ASN1;
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [
-      {
-        find: 'adem-chrome',
-        replacement: fileURLToPath(new URL('../adem-chrome/lib/index.ts', import.meta.url)),
-      },
-      {
-        find: /^@lapo\/asn1js$/,
-        replacement: fileURLToPath(new URL('./src/vendor/asn1js.ts', import.meta.url)),
-      },
-    ],
-  },
-  optimizeDeps: {
-    include: ['ipaddr.js', 'jose', 'rfc4648'],
-    exclude: ['adem-chrome'],
-  },
 });
